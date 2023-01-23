6a46dbd6a87e037b727432aa8699d9d75051ffa6
Shaik Azad <120930148+Azad99-9@users.noreply.github.com>
2023-01-23 15:50:19 +0530

    Add files via upload

---
+++ 237_Delete Node in a Linked List.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        int temp = node.next.val;
+        node.next = node.next.next;
+        node.val = temp;
+    }
+}
