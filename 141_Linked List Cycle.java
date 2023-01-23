f4e4d7aed3398dd8e29d75b1c6e4c8d3717fcb8c
Shaik Azad <120930148+Azad99-9@users.noreply.github.com>
2023-01-23 16:45:58 +0530

    Add files via upload

---
+++ 141_Linked List Cycle.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+
+        ListNode hare = head;
+        ListNode tortoise = head;
+
+        while(hare.next != null && hare.next.next != null) {
+            hare = hare.next.next;
+            tortoise = tortoise.next;
+            if(hare == tortoise) return true;
+        }
+
+        return false;
+
+        
+    }
+}
