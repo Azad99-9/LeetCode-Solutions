c646d95f54c850eb83e839c3f3987ca9d5ddadac
Shaik Azad <120930148+Azad99-9@users.noreply.github.com>
2023-02-19 14:02:19 +0530

    Add files via upload

--- 144_Binary Tree Preorder Traversal.java
+++ 144_Binary Tree Preorder Traversal.java
@@ -14,20 +14,32 @@
  * }
  */
 class Solution {
-        public void preorder(TreeNode root) {
-             // T - O( N )
-             // S - O( N )
-            if(root != null) {
-                ans.add(root.val);
-                preorder(root.left);
-                preorder(root.right);
+        // T -O( N )
+        // s -O( 1 )
+        public List<Integer> preorderTraversal(TreeNode root) {
+            ArrayList<Integer> preorder = new ArrayList<>();
+        TreeNode current = root;
+        while(current != null) {
+            if(current.left == null) {
+                preorder.add(current.val);
+                current = current.right;
+            } else {
+                TreeNode prev = current.left;
+                while(prev.right != null && prev.right != current) {
+                    prev = prev.right;
+                }
+
+                if(prev.right == null) {
+                    prev.right = current;
+                    preorder.add(current.val);
+                    current = current.left;
+                } else {
+                    prev.right = null;
+                    current = current.right;
+                }
             }
         }
-
-        List<Integer> ans = new LinkedList<>();
-        public List<Integer> preorderTraversal(TreeNode root) {
-            preorder(root);
-            return ans;
+        return preorder;
         }
     
 }
