befe5bc0a3af966f3a6ede4f8c2c28ef15674327
Shaik Azad <120930148+Azad99-9@users.noreply.github.com>
2023-03-02 15:03:32 +0530

    Add files via upload

---
+++ 653_Two Sum IV - Input is a BST.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class BSTIterator {
+    private Stack<TreeNode> stack = new Stack<TreeNode>();
+    boolean reverse = true;
+    // S - O( H )
+
+    public BSTIterator(TreeNode root, boolean isReverse) {
+        reverse = isReverse;
+        pushAll(root);
+    }
+    
+    public int next() {
+        // T - O( 1 )
+        TreeNode tmpNode = stack.pop();
+        if(!reverse) pushAll(tmpNode.right);
+        else pushAll(tmpNode.left);
+        return tmpNode.val;
+    }
+    
+    public boolean hasNext() {
+        // T - O( 1 )
+        return !stack.isEmpty();
+    }
+
+    private void pushAll(TreeNode node) {
+        // T - O( H )
+        while(node != null) {
+            stack.push(node);
+            if(reverse)
+                node = node.right;
+            else
+                node = node.left;
+        }
+    }
+}
+
+
+class Solution {
+    HashSet<Integer> tracker = new HashSet();
+    public boolean findTarget(TreeNode root, int k) {
+        if(root == null) return false;
+        BSTIterator l = new BSTIterator(root, false);
+        BSTIterator r = new BSTIterator(root, true); 
+
+        int i = l.next();
+        int j = r.next();
+        while(i < j) {
+            if(i + j == k) return true;
+            else if(i + j < k) i = l.next();
+            else j = r.next();
+        }
+
+        return false;
+    }
+}
