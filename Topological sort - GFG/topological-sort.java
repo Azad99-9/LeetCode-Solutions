b1887809bfbd1786818e0823409aacce5cf1b029
Shaik Azad <120930148+Azad99-9@users.noreply.github.com>
2023-03-07 14:12:37 +0530

    Added solution - LeetHub

---
+++ Topological sort - GFG/topological-sort.java
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            String st[] = read.readLine().trim().split("\\s+");
+            int edg = Integer.parseInt(st[0]);
+            int nov = Integer.parseInt(st[1]);
+
+            for (int i = 0; i < nov + 1; i++)
+                list.add(i, new ArrayList<Integer>());
+
+            int p = 0;
+            for (int i = 1; i <= edg; i++) {
+                String s[] = read.readLine().trim().split("\\s+");
+                int u = Integer.parseInt(s[0]);
+                int v = Integer.parseInt(s[1]);
+                list.get(u).add(v);
+            }
+
+            int[] res = new Solution().topoSort(nov, list);
+
+            if (check(list, nov, res) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+    static boolean check(ArrayList<ArrayList<Integer>> list, int V, int[] res) {
+        
+        if(V!=res.length)
+        return false;
+        
+        int[] map = new int[V];
+        for (int i = 0; i < V; i++) {
+            map[res[i]] = i;
+        }
+        for (int i = 0; i < V; i++) {
+            for (int v : list.get(i)) {
+                if (map[i] > map[v]) return false;
+            }
+        }
+        return true;
+    }
+}
+
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+
+class Solution
+{
+    //Function to return list containing vertices in Topological order. 
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        // S - O( N )
+        // T - ( V + E )
+        int indegree[] = new int[V];
+        for(int i = 0; i < V; i++) {
+            for(int item : adj.get(i)) 
+                indegree[item]++;
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < V; i++) {
+            if(indegree[i] == 0)
+                q.add(i);
+        }
+        
+        int topo[] = new int[V];
+        int i = 0;
+        while(!q.isEmpty()) {
+            int node = q.peek();
+            q.poll();
+            topo[i++] = node;
+            //node is in your topo sort
+            //so please remove it from indegree
+            
+            for(int item : adj.get(node)) {
+                indegree[item]--;
+                if(indegree[item] == 0) 
+                    q.add(item);
+            }
+        }
+        
+        return topo;
+    }
+}
