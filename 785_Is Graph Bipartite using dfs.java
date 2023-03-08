015658c6e2e1389da1734a768fb4601d0fc12b1f
Shaik Azad <120930148+Azad99-9@users.noreply.github.com>
2023-03-08 16:15:33 +0530

    Add files via upload

---
+++ 785_Is Graph Bipartite using dfs.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isBipartite(int[][] adj)
+    {
+        int V = adj.length;
+        int color[] = new int[V];
+	    for(int i = 0;i<V;i++) color[i] = -1; 
+	    
+	    // for connected components
+	    for(int i = 0;i<V;i++) {
+	        if(color[i] == -1) {
+	            if(dfs(i, 0, color, adj) == false) return false; 
+	        }
+	    }
+	    return true; 
+    }
+
+     private boolean dfs(int node, int col, int color[], 
+    int[][] adj) {
+        
+        color[node] = col; 
+        
+        // traverse adjacent nodes
+        for(int it : adj[node]) {
+            // if uncoloured
+            if(color[it] == -1) {
+                if(dfs(it, 1 - col, color, adj) == false) return false; 
+            }
+            // if previously coloured and have the same colour
+            else if(color[it] == col) {
+                return false; 
+            }
+        }
+        
+        return true; 
+    }
+}
