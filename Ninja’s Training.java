31d08d54b547c536505cf639209a1faf8406e01d
Shaik Azad <120930148+Azad99-9@users.noreply.github.com>
2023-03-13 13:46:29 +0530

    Add files via upload

---
+++ Ninja’s Training.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public static int ninjaTraining(int n, int points[][]) {
+    	// T - O ( N )
+    	// S - O ( 1 )
+
+        int[] prev = new int[4];
+
+        prev[0] = Math.max(points[0][1], points[0][2]);
+        prev[1] = Math.max(points[0][0], points[0][2]);
+        prev[2] = Math.max(points[0][0], points[0][1]);
+        prev[3] = Math.max(points[0][1], Math.max(points[0][0],points[0][2]));
+
+        for (int day = 1; day < n; day++) {
+            int[] temp = new int[4];
+            for (int last = 0; last < 4; last++) {
+                temp[last] = 0;
+
+                for (int task = 0; task < 3; task++) {
+                    if (task != last) {
+                        temp[last] = Math.max(temp[last], points[day][task] + prev[task]);
+                    }
+                }
+            }
+
+            prev = temp;
+        }
+
+        return prev[3];
+
+
+    }
+
+}
