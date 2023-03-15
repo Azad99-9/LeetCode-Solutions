e6222f57119f7d4e359c28fa2e4ec819a2eaa9bb
Shaik Azad <120930148+Azad99-9@users.noreply.github.com>
2023-03-15 12:20:26 +0530

    Add files via upload

---
+++ 1463_Cherry Pickup II.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int cherryPickup(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][][] dp = new int[n][m][m];
+        for (int j1 = 0; j1 < m; j1++) {
+            for (int j2 = 0; j2 < m; j2++) {
+                if (j1 == j2) dp[n - 1][j1][j2] = grid[n - 1][j1];
+                else dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j1 = 0; j1 < m; j1++) {
+                for (int j2 = 0; j2 < m; j2++) {
+                    int maxi = (int)-1e9;
+                    for (int dj1 = -1; dj1 <= 1; dj1++) {
+                        for (int dj2 = -1; dj2 <= 1; dj2++) {
+                            int value = 0;
+                            if (j1 == j2) value = grid[i][j1];
+                            else value = grid[i][j1] + grid[i][j2];
+                            if (j1 + dj1 >= 0 && j1 + dj1 < m && j2 + dj2 >= 0 && j2 + dj2 < m)
+                                value += dp[i + 1][j1 + dj1][j2 + dj2];
+                            else value += -1e9;
+                            maxi = Math.max(maxi, value);
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][m - 1];
+
+    }
+}
