73608888a8ebe2eadb99b9701478146de1235da5
Shaik Azad <120930148+Azad99-9@users.noreply.github.com>
2023-03-20 10:58:26 +0530

    Add files via upload

---
+++ 139_Word Break.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[s.length()] = true;
+
+        for (int i = s.length() - 1; i > -1; i-- ) {
+            for (String word : wordDict) {
+                if (i + word.length() <= s.length() && s.substring(i, i + word.length()).equals(word)) {
+                    dp[i] = dp[i + word.length()];
+                }
+                if (dp[i])
+                    break;
+            }
+        }
+        return dp[0];
+    }
+}
