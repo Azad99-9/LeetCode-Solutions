06e89087a90db426b2502725bb8474c760620e97
Shaik Azad <120930148+Azad99-9@users.noreply.github.com>
2023-03-27 12:11:54 +0530

    Added solution - LeetHub

---
+++ Pair with given sum in a sorted array - GFG/pair-with-given-sum-in-a-sorted-array.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+            
+
+class Solution{
+    
+  
+    int Countpair(int a[], int n, int sum)
+    {
+        int low = 0;
+        int high = n - 1;
+        int count = 0;
+        while (low < high) {
+            int curentPairSum = a[low] + a[high];
+            if (curentPairSum < sum) low++;
+            else {
+                if (curentPairSum == sum) count++;
+                high--;
+            }
+        }
+        if(count == 0) return -1;
+        return count;
+    }
+  
+    
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    
+		    String line1 = br.readLine();
+		    String[] element = line1.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    int K = Integer.parseInt(br.readLine());
+		    
+		    Solution obj = new Solution();
+		   
+		    int res = obj.Countpair(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		    
+		    
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
