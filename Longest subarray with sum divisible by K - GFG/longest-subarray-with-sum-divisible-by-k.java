4bed5414d2a71f151ea9fd78cce22ad0f9659250
Shaik Azad <120930148+Azad99-9@users.noreply.github.com>
2023-03-30 18:56:34 +0530

    Added solution - LeetHub

---
+++ Longest subarray with sum divisible by K - GFG/longest-subarray-with-sum-divisible-by-k.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    int longSubarrWthSumDivByK(int a[], int n, int k)
+    {
+        Map<Integer, Integer> mod = new HashMap<>();
+        mod.put(0, -1);
+        int sum = 0;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            sum += a[i];
+            int rem = sum % k;
+            if (rem < 0) rem += k;
+            
+            if (!mod.containsKey(rem)) mod.put(rem, i); // not seen before in map
+            else ans = Math.max(ans, i - mod.get(rem)); // seen before in map
+        }
+        return ans;
+       
+    }
+ 
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class GFG {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    
+		    String line1 = br.readLine();
+		    String[] element = line1.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		   
+		    int res = obj.longSubarrWthSumDivByK(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		    
+		    
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
