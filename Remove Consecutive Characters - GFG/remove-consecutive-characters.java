308c0d7ec8d722da76e942526d95349080f8d1cd
Shaik Azad <120930148+Azad99-9@users.noreply.github.com>
2023-04-01 21:41:08 +0530

    Added solution - LeetHub

---
+++ Remove Consecutive Characters - GFG/remove-consecutive-characters.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            
+            String S = read.readLine().trim();
+            
+            Solution ob = new Solution();
+            String ans = ob.removeConsecutiveCharacter(S);
+            
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public String removeConsecutiveCharacter(String S){
+        StringBuilder str = new StringBuilder(S);
+        int i = 1;
+        while (i < str.length()) {
+            if (str.charAt(i) == str.charAt(i - 1)) str.deleteCharAt(i);
+            else i++;
+        }
+        return str.toString();
+    }
+}
