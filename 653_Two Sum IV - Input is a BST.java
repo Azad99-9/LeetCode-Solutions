7f92a1e40e436578700d61bfeb2a56a2c7c4ba8c
Azad99-9 <shaikazad2003@gmail.com>
2023-06-11 23:30:21 +0530

    cover up

--- 653_Two Sum IV - Input is a BST.java
+++ 653_Two Sum IV - Input is a BST.java
@@ -1,18 +1,4 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 public class BSTIterator {
     private Stack<TreeNode> stack = new Stack<TreeNode>();
     boolean reverse = true;
