fcc303a9e73cd1969ab13e1ab9eba373b8371714
Azad99-9 <shaikazad2003@gmail.com>
2023-11-26 00:13:04 +0530

    potd 128

---
+++ POTD/1685. Sum of Absolute Differences in a Sorted Array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int[] prefixSum = new int[n];
+        int[] suffixSum = new int[n];
+
+        // Calculate and initialize the prefix sums & suffixSum array
+        prefixSum[0] = nums[0];
+        suffixSum[n - 1] = nums[n - 1];
+
+        // Calculate the prefix sums & suffixSum array in one loop
+        for (int i = 1; i < n; ++i) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+            suffixSum[n - i - 1] = suffixSum[n - i] + nums[n - i - 1];
+        }
+
+        // Calculate absolute differences and update the result array
+        for (int i = 0; i < n; ++i) {
+            int currentAbsoluteDiff = ((nums[i] * i) - prefixSum[i]) + (suffixSum[i] - (nums[i] * (n - i - 1)));
+            result[i] = currentAbsoluteDiff;
+        }
+
+        return result;
+    }
+}
