60342dc1ef5e3636c22c8282a78487cfe8e75dc3
Azad99-9 <shaikazad2003@gmail.com>
2023-11-29 23:39:10 +0530

    potd 131

---
+++ POTD/Euler circuit and Path.java
@@ -0,0 +1,15 @@
+class Solution{
+    public int isEulerCircuit(int V, List<Integer>[] adj) 
+    {
+        // code here
+       int oddCount = 0;
+        for(int i=0;i<V;i++){ 
+            if(adj[i].size()%2!=0){
+                oddCount+=1;
+        }
+        }
+        if(oddCount==0)return 2;
+        else if(oddCount==2)return 1;
+        else return 0;
+    }
+}
